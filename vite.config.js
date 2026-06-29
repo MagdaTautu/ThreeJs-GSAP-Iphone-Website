069235028c7962a604f4b495ba890dd08171932b
MagdaTautu <tautu_magda@yahoo.com>
2026-06-29 11:48:44 +0300

    finish

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/3D-Iphone-Website/',
   plugins: [
     tailwindcss(),
   ],
